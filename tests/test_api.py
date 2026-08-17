aea0044391660f108289016482b6c47c7f0ca44d
Avi Soboroff <avi.soboroff@gmail.com>
2026-08-17 19:28:34 -0400

    first two automated test cases created

---
+++ tests/test_api.py
@@ -0,0 +1,22 @@
+from fastapi.testclient import TestClient
+from main import app
+
+client = TestClient(app)
+
+#  Test for missing fields a.k.a failed POST request
+def test_add_profile_missing_fields():
+    response = client.post("/profiles", json={})
+    assert response.status_code == 422
+    assert "detail" in response.json()
+
+# Test for a successful POST request. Can adjust the content field to include any string of any size
+def test_add_profile_with_fields():
+    payload = {"user_name": "TestUser", "content": "This is valid profile content"}
+    # removes whitespaces and splits content string by new paragraph lines if there is content after the newline.
+    chunks = len(c.strip() for c in payload["content"].split('\n\n') if c.strip())
+    response = client.post("/profiles", json=payload)
+
+    assert response.status_code == 200
+    response_data = response.json()
+    assert response_data["user_name"] == payload["user_name"]
+    assert response_data["chunks_added"] == chunks+1
