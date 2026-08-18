bd6d01bac704934db3d80f96f92514c36bfc97f7
Avi Soboroff <avi.soboroff@gmail.com>
2026-08-17 20:03:01 -0400

    3rd test case added

--- tests/test_api.py
+++ tests/test_api.py
@@ -13,10 +13,28 @@ def test_add_profile_missing_fields():
 def test_add_profile_with_fields():
     payload = {"user_name": "TestUser", "content": "This is valid profile content"}
     # removes whitespaces and splits content string by new paragraph lines if there is content after the newline.
-    chunks = len(c.strip() for c in payload["content"].split('\n\n') if c.strip())
+    chunks = len([c.strip() for c in payload["content"].split('\n\n') if c.strip()])
     response = client.post("/profiles", json=payload)
 
     assert response.status_code == 200
     response_data = response.json()
     assert response_data["user_name"] == payload["user_name"]
-    assert response_data["chunks_added"] == chunks+1
+    assert response_data["chunks_added"] == chunks
+
+# # Test GET endpoint when user parameter is used
+# def test_ask_with_user_filter():
+#     params = {"question": "What do you want to know?", "user": "TestUser"}
+#     response = client.get("/ask", params=params)
+
+#     assert response.status_code == 200
+#     response_data = response.json()
+
+# Test GET endpoint without user parameter
+def test_ask_without_user_filter():
+    params = {"question": "What do you want to know?"}
+    response = client.get("/ask", params=params)
+
+    assert response.status_code == 200
+    response_data = response.json()
+    assert response_data["question"] == params["question"]
+    assert response_data["filtered_by_user"] is None
